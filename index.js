88e139cc4783cf4bea6b7419f9f5b0b9f0a92cf9
Hataipan Weerapin <nutpedd23@hotmail.com>
2019-07-13 14:14:07 +0700

    เพิ่มหน้า test

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hataipan Weerapin'))
 app.get('/about', (req, res) => res.send('หน้า about'))
-app.get('/context', (req, res) => res.send('หน้า context'))
+app.get('/contact', (req, res) => res.send('หน้า contact'))
+app.get('/test', (req, res) =>{
+    res.json({message : "test"})
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
